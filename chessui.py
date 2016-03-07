e110e7c42d67d31435c6f964be7c3fcc7269fa81
jiedo <jiedohh@gmail.com>
2016-03-07 14:00:50 +0800

    feature: add user interface

---
+++ chessui.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python
+#encoding: utf-8
+
+import sys
+import time
+import random
+import chess
+import math, pygame
+from pygame.locals import *
+
+
+CHESSMAN_SIZE = 30
+
+BOARD_LEFT = 2 * CHESSMAN_SIZE
+BOARD_TOP = 2 * CHESSMAN_SIZE
+
+BOARD_WIDTH = (chess.WIDTH -1)* CHESSMAN_SIZE
+BOARD_HEIGHT = (chess.HEIGHT - 1) * CHESSMAN_SIZE
+
+SCREEN_WIDTH = BOARD_WIDTH + 4 * CHESSMAN_SIZE
+SCREEN_HEIGHT = BOARD_HEIGHT + 4 * CHESSMAN_SIZE
+
+WINSIZE = [SCREEN_WIDTH, SCREEN_HEIGHT]
+
+g_point_to_put = [0, 0]
+g_show_point_to_put = [SCREEN_WIDTH/2, SCREEN_HEIGHT/2]
+
+
+def get_click_point(pos_click):
+    cw, ch = pos_click
+    cw, ch = cw + CHESSMAN_SIZE/2, ch + CHESSMAN_SIZE/2
+
+    int_cw = (cw - BOARD_LEFT + CHESSMAN_SIZE) / CHESSMAN_SIZE
+
+    int_cw_out = False
+    if int_cw < 0:
+        int_cw_out = True
+    if cw < BOARD_LEFT + int_cw * CHESSMAN_SIZE + CHESSMAN_SIZE/2:
+        int_cw -= 1
+    if int_cw < 0 or int_cw >= chess.WIDTH:
+        int_cw_out = True
+
+    int_ch_out = False
+    int_ch = (ch - BOARD_TOP + CHESSMAN_SIZE) / CHESSMAN_SIZE
+    if int_ch < 0:
+        int_ch_out = True
+    if ch < BOARD_TOP + int_ch * CHESSMAN_SIZE + CHESSMAN_SIZE/2:
+        int_ch -= 1
+    if int_ch < 0 or int_ch >= chess.HEIGHT:
+        int_ch_out = True
+
+    if int_ch_out or int_cw_out:
+        return None, None
+    return (int_cw, int_ch)
+
+
+def draw_board(surface, chess_board, color):
+    "used to draw (and clear) the stars"
+
+    line_color = 40, 80, 40
+    background_color = 100, 100, 100
+    black_color = 10, 10, 10
+    white_color = 240, 240, 240
+    click_color = 40, 80, 40
+    surface.fill(background_color)
+
+    line_width = 2
+    chess_label_font = pygame.font.SysFont("Arial", 12)
+
+    cw, ch = get_click_point(g_show_point_to_put)
+    if cw is not None and ch is not None:
+        pygame.draw.circle(surface, click_color,
+                           (BOARD_LEFT + cw * CHESSMAN_SIZE,
+                            BOARD_TOP + ch * CHESSMAN_SIZE),
+                           CHESSMAN_SIZE/2, line_width)
+
+    for h in range(chess.HEIGHT):
+        start_pos = (BOARD_LEFT,
+                     BOARD_TOP + h * CHESSMAN_SIZE,)
+        end_pos = (BOARD_LEFT + BOARD_WIDTH,
+                     BOARD_TOP + h * CHESSMAN_SIZE,)
+        pygame.draw.line(surface, line_color, start_pos, end_pos, line_width)
+
+        vertical_label = str(chess.HEIGHT - h)
+        #AA, no transparancy, bold
+        fg = 250, 240, 230
+        ren = chess_label_font.render(vertical_label, 1, fg, background_color)
+        vertical_label_size = chess_label_font.size(vertical_label)
+        surface.blit(ren, (BOARD_LEFT - vertical_label_size[0] - CHESSMAN_SIZE/2,
+                           BOARD_TOP + h * CHESSMAN_SIZE - vertical_label_size[1]/2))
+
+    for w in range(chess.WIDTH):
+        start_pos = (BOARD_LEFT + w * CHESSMAN_SIZE,
+                     BOARD_TOP,)
+        end_pos = (BOARD_LEFT + w * CHESSMAN_SIZE,
+                     BOARD_TOP + BOARD_HEIGHT,)
+        pygame.draw.line(surface, line_color, start_pos, end_pos, line_width)
+
+        horizontal_label = chess.idtoa(w)
+        #AA, no transparancy, bold
+        fg = 250, 240, 230
+        ren = chess_label_font.render(horizontal_label, 1, fg, background_color)
+        horizontal_label_size = chess_label_font.size(horizontal_label)
+        surface.blit(ren, (BOARD_LEFT + w * CHESSMAN_SIZE - horizontal_label_size[0]/2,
+                           BOARD_TOP + BOARD_HEIGHT + CHESSMAN_SIZE/2))
+
+    for h in range(chess.HEIGHT):
+        for w in range(chess.WIDTH):
+            test_side = chess_board[chess.HEIGHT-h-1][w]
+            if test_side == chess.BLANK:
+                continue
+
+            pos = (BOARD_LEFT + w * CHESSMAN_SIZE,
+                   BOARD_TOP + h * CHESSMAN_SIZE,)
+
+            if test_side == chess.BLACK:
+                color = black_color
+            elif test_side == chess.WHITE:
+                color = white_color
+            radius = int(CHESSMAN_SIZE * 0.309)
+            width = radius
+            pygame.draw.circle(surface, color, pos, radius, width)
+
+
+def main():
+    # 默认实现成回调strategy()模式,
+    # 但可以实现成更复杂模式, 符合bot通信协议即可
+
+    global g_point_to_put, g_show_point_to_put
+
+    sleep_time = 0.01
+    show_verbose = False
+    chess.g_debug_info = False
+    if len(sys.argv) >= 2:
+        if "-w" in sys.argv:
+            #如果选择白方, 则通知对方START.
+            chess.chess_operate("START")
+
+        if "-v" in sys.argv:
+            show_verbose = True
+
+        if "-d" in sys.argv:
+            chess.g_debug_info = True
+
+    ################
+    #create our starfield
+    random.seed()
+    clock = pygame.time.Clock()
+    #initialize and prepare screen
+    pygame.init()
+    screen = pygame.display.set_mode(WINSIZE)
+    pygame.display.set_caption('Gobang')
+    white = 255, 240, 200
+    done = False
+    ################
+
+    bot = chess.Bot()
+    while True:
+        ################
+        draw_board(screen, bot.board, white)
+        pygame.display.update()
+        for e in pygame.event.get():
+            if e.type == QUIT or (e.type == KEYUP and e.key == K_ESCAPE):
+                done = True
+                break
+            elif e.type == MOUSEMOTION:
+                g_show_point_to_put = list(e.pos)
+            elif e.type == MOUSEBUTTONDOWN and e.button == 1:
+                g_point_to_put[:] = list(e.pos)
+                g_show_point_to_put = list(e.pos)
+        if done: break
+        clock.tick(10)
+        ################
+
+        # 首先读取对方的落子位置, 并写入棋盘
+        while bot.b_next == bot.b_your_side:
+            h, w = bot.chess_get()
+
+        # 检测对方是否获胜
+        if bot.is_winner(bot.b_your_side):
+            bot.light_on_win_points()
+            chess.chess_log("Notes: %d" % (len(bot.notes)))
+            time.sleep(0.1)
+            bot.show_board()
+            break
+
+        # 回调自己的策略
+        cw, ch = get_click_point(g_point_to_put)
+        if cw is None or ch is None:
+            continue
+
+        g_point_to_put = [0, 0]
+        h = chess.HEIGHT - ch - 1
+        w = cw
+        #h, w = strategy(bot)
+        # 写入棋盘并通知对方
+        bot.chess_put(h, w)
+        if show_verbose:
+            time.sleep(sleep_time/10)
+            bot.show_board()
+            time.sleep(sleep_time)
+
+        # 检测自己是否获胜
+        if bot.is_winner(bot.b_my_side):
+            chess.chess_log("%s Win." % bot.b_my_side)
+            break
+
+
+def strategy(self):
+    # 测试AI
+    if self.b_my_side == chess.BLACK:
+        return strategy4(self, 0, True, True)
+    else:
+        return strategy4(self, 0, True, True)
+
+
+def strategy4(self, defence_level, is_dup_enforce, is_space_enough):
+    # 测试AI
+    # is_dup_enforce: 连珠对附近空白是否有加分
+    # is_space_enough: 是否检查空白处扩展空间足够WIN_NUM
+    # defence_level: 防御权重, 越大越重视防御
+    #
+    # 统计双方所有棋子米字形线条交汇计数最高的空白
+    # max(points_score) = max(max(your's + defence),  max(mine))
+    all_my_blank_points_count_pair = self.get_score_of_blanks_side(self.b_my_side,
+                                                                   dup=is_dup_enforce,
+                                                                   test_space=is_space_enough)
+    all_your_blank_points_count_pair = self.get_score_of_blanks_side(self.b_your_side,
+                                                                   dup=is_dup_enforce,
+                                                                   test_space=is_space_enough)
+    if all_your_blank_points_count_pair:
+        your_pt, your_max_count = all_your_blank_points_count_pair[0]
+        if all_my_blank_points_count_pair:
+            my_pt, my_max_count = all_my_blank_points_count_pair[0]
+            if defence_level + your_max_count <= my_max_count:
+                candidates = [pt for pt, count in all_my_blank_points_count_pair if count == my_max_count]
+                return random.choice(candidates)
+
+        candidates = [pt for pt, count in all_your_blank_points_count_pair if count == your_max_count]
+        return random.choice(candidates)
+
+    return (chess.HEIGHT/2, chess.WIDTH/2)
+
+
+# if python says run, then we should run
+if __name__ == '__main__':
+    main()
